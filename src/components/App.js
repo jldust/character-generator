9b6c73ece4d681a07a779927f111177248559c94
Jennifer Dust <dust.jennifer@gmail.com>
2020-07-09 18:42:13 -0500

    Testing stored data

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 //Get Compontents & Style
 import '../css/App.scss';
-import SelectedData from './SelectedData';
+//import SelectedData from './SelectedData';
 //import Footer from './footer'
 import Button from 'react-bootstrap/Button';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -11,12 +11,131 @@ import { Container, Row, Col } from 'react-bootstrap';
 //Get possible Charater Data
 import data from '../data/characters.json'
 
-//Current Character data stored
-class savedCharacter extends Component {
+
+
+
+//Prompt user if they would like to store any of the current values and roll again
+//Select parts of character to keep and re-generate the rest
+class SelectedData extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //Data for dynamic checkboxes
+      categories: [
+        {
+          id: 'race', 
+          value: "Keep Current Race"
+        },
+        {
+          id: 'class', 
+          value: "Keep Current Class"
+        },
+        {
+          id: 'background', 
+          value: "Keep Current Background"
+        }
+      ],
+      //Data for stored data
+      savedData: [
+        {
+          id: 'race', 
+          value: ""
+        },
+        {
+          id: 'class', 
+          value: ""
+        },
+        {
+          id: 'background', 
+          value: ""
+        }
+      ],
+      checkedItems: new Map ()
+    }
+
+    //Attach Handlers for Checkboxes
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  //Something happened with the Checkbox
+  handleChange(event) {
+    var isChecked = event.target.checked;
+    var item = event.target.value;
+
+    this.setState(prevState => ({ checkedItems: prevState.checkedItems.set(item, isChecked) }));
+
+    //Only write data if box is checked
+    switch(item){
+      case 'race':
+        if(isChecked === true)
+          console.log(this.props);
+          //this.setState(id: this.props);
+
+        console.log(isChecked);
+        break;
+      case 'class':
+        if(isChecked === true)
+          console.log("class true");
+        console.log(isChecked);
+        break;
+      case 'background':
+        if(isChecked === true)
+          console.log("Back true");
+        console.log(isChecked);
+        break;
+      default:
+        return;
+    }
+
+  }
+
+
+  //Want to update character generated
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("See me?");
+  }
+
+
   render() {
-    const currentChar = this.props.character;
     return (
-      <p>This is your current character: A {currentChar} </p>
+      <Container>
+        <form onSubmit={this.handleSubmit}>
+          <Row>{
+            this.state.categories.map((item) => (
+              <Col key={item.id}>
+                <label>
+                  <input type="checkbox" value={item.id} onChange={this.handleChange} /> {item.value}
+                </label>
+              </Col>
+            ))
+          }
+          </Row>
+          <Row>
+            <Col>
+              <br />
+              <input type="submit" value="Re-Generate with Seletions" />
+            </Col>
+          </Row>
+        </form>
+        <Row>
+          Here is the current stored data: 
+        </Row>
+    </Container>
+    );
+  }
+}
+
+
+//Current Character data stored
+class SavedCharacter extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <p>This is your current character: A </p>
+        </Row>
+          </div>
     );
   }
 }
@@ -26,13 +145,29 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
+      //Hold randomed data
+      currentChar: {
         race: "",
         charClass: "",
-        background: "",
-      };
+        background: ""
+      },
+      //Data for stored data
+      savedData: [
+        {
+          id: 'race', 
+          value: ""
+        },
+        {
+          id: 'class', 
+          value: ""
+        },
+        {
+          id: 'background', 
+          value: ""
+        }
+      ]
+    };
   
-    //Constant to hold current data
-    //var currentChar = {savedRace: "", savedClass: "", savedBackground: ""};
     //Bind Checkboxes & Generator Button
     this.handleGenerateCharacter = this.handleGenerateCharacter.bind(this);
   }
